8bfd3dc8e3eee9b3f683df239c10fc1176984ea5
xiaoerlong <Tronsis@json-wifi.local>
2017-09-28 10:49:14 +0800

    first commit

---
+++ XEL_Player/XEL_Player/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XEL_Player
+//
+//  Created by Tronsis－mac on 17/9/28.
+//  Copyright © 2017年 Kim－pc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XEL_Player/XEL_Player/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XEL_Player
+//
+//  Created by Tronsis－mac on 17/9/28.
+//  Copyright © 2017年 Kim－pc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
